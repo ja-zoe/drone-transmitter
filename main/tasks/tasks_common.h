ed37c4e25a155f1acf9ae087f4fc275ef68f3f7e
Julian V <j.vilfort01@gmail.com>
2026-05-04 22:11:03 -0400

    Remove old drone_common submodule registration

--- main/tasks/tasks_common.h
+++ main/tasks/tasks_common.h
@@ -1,23 +1,6 @@
 #pragma once
 #include "drone_common.h"
 
-/*----- Task pvParameters -----*/
-typedef struct {
-    control_packet_t *control_packet;
-    SemaphoreHandle_t lock;
-} input_task_params_t;
-
-typedef struct {
-  telemetry_packet_t *telemetry_packet;
-  SemaphoreHandle_t lock;
-} oled_task_params_t;
-
-typedef struct {
-  telemetry_packet_t *telemetry_packet;
-  SemaphoreHandle_t lock;
-  uint8_t *des_addr;
-} receive_task_params_t;
-
 /*----- Task function definitions -----*/
 void inputReadTask( void *pvParameters);
 void dataTransmitTask( void *pvParameters );
